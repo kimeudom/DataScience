bc5005bc95577e097b52269263e03aeeacce83af
Dominic <kimeudom02@gmail.com>
2022-03-24 09:44:30 +0300

    fixed malaria data

--- t question formulation/population_densities.r
+++ t question formulation/population_densities.r
@@ -1,4 +1,7 @@
 #  The population densities
 Population <- read.table(file = "/home/dom/Data_Science/t\ question\ formulation/new_csv/Population\ data.csv",
                       sep = "," ,header = TRUE, skipNul = FALSE, fill = TRUE)
+
+names(Population) <- c("County_code", "County", "Population")
+
                       
