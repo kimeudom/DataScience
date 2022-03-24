562cd35d112cdc653dece9fc2944c1b84255ad93
Dominic <kimeudom02@gmail.com>
2022-03-24 09:24:53 +0300

    additions

--- t question formulation/malaria_spread_montly.r
+++ t question formulation/malaria_spread_montly.r
@@ -5,6 +5,26 @@
 #The Malaria data set
 Malaria <- read.table(file = "/home/dom/Data_Science/t\ question\ formulation/new_csv/KHIS\ Malaria\ Data.csv",
                      sep = "," ,header = TRUE, skipNul = FALSE, fill = TRUE)
-
-a <- order(colSums(Malaria[,5:17]))
+## Truncate Malaria for confirmed cases only
+Malaria_conf <- Malaria[Malaria$Data.ID == "OoakJhWiyZp",]
+Mal_output <- cbind(
+  County = Malaria$Orgunit.name,
+  Jan = Malaria$X21.Jan,
+  Feb = Malaria$X21.Feb,
+  Mar = Malaria$X21.Mar,
+  Apr = Malaria$X21.Apr,
+  May = Malaria$X21.May,
+  June = Malaria$X21.Jun,
+  July = Malaria$X21.Jul,
+  Aug = Malaria$X21.Aug,
+  Sep = Malaria$X21.Sep,
+  Oct = Malaria$X21.Oct,
+  Nov = Malaria$X21.Nov,
+  Dec = Malaria$X21.Dec
+  
+)
+## Make output
+write.table(Mal_output,
+            file = "/home/dom/Data_Science/t\ question\ formulation/Outputs/Mal_Spread_monthly.csv",
+            sep = ",", quote = FALSE, append = FALSE, na = "NA", row.names = FALSE)
 
---
+++ t question formulation/population_densities.r
@@ -0,0 +1,4 @@
+#  The population densities
+Population <- read.table(file = "/home/dom/Data_Science/t\ question\ formulation/new_csv/Population\ data.csv",
+                      sep = "," ,header = TRUE, skipNul = FALSE, fill = TRUE)
+                      
